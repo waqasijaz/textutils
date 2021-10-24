0de14bd1ca7778bc8c7517f76ff6e44b02660fa5
waqasijaz <waqasijazx6@gmail.com>
2021-10-24 23:50:45 +0500

    Routing

--- src/App.js
+++ src/App.js
@@ -1,15 +1,46 @@
-import logo from "./logo.svg";
+// import logo from "./logo.svg";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
+import About from "./components/About";
+import { useState } from "react/cjs/react.development";
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
+  const [mode, setMode] = useState("light"); // To check if dark mode is enabled or not
+
+  const toggleMode = () => {
+    if (mode === "light") {
+      setMode("dark");
+      document.body.style.backgroundColor = "#042743";
+      // document.body.text.color
+    } else {
+      setMode("light");
+      document.body.style.backgroundColor = "white";
+    }
+  };
   return (
     <>
-  <Navbar title="TextUtils App" about="About App"/>
-  <div className="container">
-  <TextForm heading="Enter Your Text Here To Analyze"/>
-  </div>
+    <Router>
+      <Navbar
+        title="TextUtils App"
+        about="About App"
+        mode={mode}
+        toggleMode={toggleMode}
+      />
+       
+      <div className="container">
+        <Switch>
+          <Route exact path="/about">
+            <About />
+          </Route>
+          <Route path="/">
+            <TextForm heading="Enter Your Text Here To Analyze" mode={mode} />
+          </Route>
+        </Switch>
+      </div>
+      </Router>
     </>
   );
 }
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -30,7 +30,7 @@ setText(newText);
 
     return (
         <>
-    <div className="container my-4">
+    <div className="container my-4" style={{color: props.mode ==="dark"?"white":"black"}}>
         <h1>{props.heading}</h1>
       <form>
         <div className="mb-3">
@@ -40,6 +40,7 @@ setText(newText);
             rows="8"
             value={text}
             onChange={handleOnChange}
+            style={{backgroundColor: props.mode ==="dark"?"grey":"white",color: props.mode ==="dark"?"white":"black"}}
           ></textarea>
         </div>
       </form>
@@ -47,12 +48,12 @@ setText(newText);
       <button className="btn btn-primary mx-2" onClick={handleLoClick}>Convert To Lower Case</button>
       <button className="btn btn-primary mx-2" onClick={handleClearClick}>Clear</button>
     </div>
-    <div className="container my-3">
+    <div className="container my-3" style={{color: props.mode ==="dark"?"white":"black"}}>
         <h1>Your Text Summary</h1>
         <p>Words {text.split(" ").length} Character {text.length}</p>
         <p>{0.008 * text.split(" ").length} Minutes required to read</p>
         <h2>Preview</h2>
-        <p>{text}</p>
+        <p>{text.length>0?text:"Enter Something To Preview..."}</p>
 
     </div>
     </>
